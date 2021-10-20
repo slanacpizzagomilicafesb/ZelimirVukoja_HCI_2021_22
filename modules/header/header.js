6238838258ee91188c047c39ae31aa0191347b4e
Mario Cagalj <mario.cagalj@fesb.hr>
2021-10-20 11:47:24 +0200

    Sync with group 2 (note: hello -> home)

--- modules/header/header.js
+++ modules/header/header.js
@@ -3,30 +3,18 @@ import styles from './header.module.css';
 import Image from 'next/image';
 
 import LogoImg from '../../assets/logo.png';
-import HeroImg from '../../assets/hero.png';
 
 const Header = () => {
     return (
         <header className={styles.main}>
             <main className={styles.content}>
-                <div className={styles.logoImg}>
-                    <Image
-                        src={LogoImg}
-                        layout="fixed"
-                        width={50}
-                        height={50}
-                        alt="Design logo"
-                    />
-                </div>
-                {/* <div className={styles.heroImg}>
-                    <Image
-                        src={HeroImg}
-                        layout="fill"
-                        objectFit="cover"
-                        objectPosition="center"
-                        alt="Design logo"
-                    />
-                </div> */}
+                <Image
+                    src={LogoImg}
+                    layout="fixed"
+                    width={50}
+                    height={50}
+                    alt="Design logo"
+                />
                 <nav className={styles.landingText}>
                     <li>Home</li>
                     <li>About Us</li>
