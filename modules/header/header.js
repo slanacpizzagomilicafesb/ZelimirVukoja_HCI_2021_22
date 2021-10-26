22215d814ac2252dadf0b7a37e5db14e171afb7d
kula <kula124@gmail.com>
2021-10-26 22:45:13 +0200

    starting point

--- modules/header/header.js
+++ modules/header/header.js
@@ -3,26 +3,41 @@ import styles from './header.module.css';
 import Image from 'next/image';
 
 import LogoImg from '../../assets/logo.png';
+import HeroImg from '../../assets/hero.png';
 
 const Header = () => {
     return (
         <header className={styles.main}>
-            <main className={styles.content}>
+            <div className={styles.bgImg}>
                 <Image
-                    src={LogoImg}
-                    layout="fixed"
-                    width={50}
-                    height={50}
-                    alt="Design logo"
+                    src={HeroImg}
+                    layout="fill"
+                    objectFit="cover"
+                    alt="Hero image"
                 />
-                <nav className={styles.landingText}>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Showcase</li>
-                    <li>Blog</li>
-                    <li>Contact</li>
-                    <li>Sign In</li>
-                </nav>
+            </div>
+            <main className={styles.content}>
+                <section className={styles.navigation}>
+                    <Image
+                        src={LogoImg}
+                        layout="fixed"
+                        width={150}
+                        height={150}
+                        alt="Design logo"
+                    />
+                    <nav>
+                        <li>Home</li>
+                        <li>About Us</li>
+                        <li>Showcase</li>
+                        <li>Blog</li>
+                        <li>Contact</li>
+                        <li>Sign In</li>
+                    </nav>
+                </section>
+                <section className={styles.titles}>
+                    <h1>Design Matters</h1>
+                    <h3>Entrust us with your digital appearance</h3>
+                </section>
             </main>
         </header>
     );
