fe6d7f216e3558d367ce3f0f580f5c19453c53fb
SlanacPizzaGomilicaFesb <zvukoj00@fesb.hr>
2021-10-27 09:16:01 +0200

    header changes

--- modules/header/header.js
+++ modules/header/header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import styles from './header.module.css';
 import Image from 'next/image';
 
 import LogoImg from '../../assets/logo.png';
@@ -7,8 +6,8 @@ import HeroImg from '../../assets/hero.png';
 
 const Header = () => {
     return (
-        <header className={styles.main}>
-            <div className={styles.bgImg}>
+        <header className="h-4/6 relative flex justify-center items-center">
+            <div className="z--1">
                 <Image
                     src={HeroImg}
                     layout="fill"
@@ -16,8 +15,8 @@ const Header = () => {
                     alt="Hero image"
                 />
             </div>
-            <main className={styles.content}>
-                <section className={styles.navigation}>
+            <main className="my-0 mx-auto flex flex-col min-w-sm max-w-6xl w-full">
+                <section className="flex justify-between items-center">
                     <Image
                         src={LogoImg}
                         layout="fixed"
@@ -25,18 +24,34 @@ const Header = () => {
                         height={150}
                         alt="Design logo"
                     />
-                    <nav>
-                        <li>Home</li>
-                        <li>About Us</li>
-                        <li>Showcase</li>
-                        <li>Blog</li>
-                        <li>Contact</li>
-                        <li>Sign In</li>
+                    <nav className="inline-flex justify-between list-none w-max">
+                        <li className="w-36 py-4 font-bold text-2xl text-center text-white bg-purple-900">
+                            Home
+                        </li>
+                        <li className="w-36 py-4 font-normal text-2xl text-center text-purple-900 hover:text-white hover:bg-purple-900">
+                            About Us
+                        </li>
+                        <li className="w-36 py-4 font-normal text-2xl text-center text-purple-900 hover:text-white hover:bg-purple-900">
+                            Showcase
+                        </li>
+                        <li className="w-36 py-4 font-normal text-2xl text-center text-purple-900 hover:text-white hover:bg-purple-900">
+                            Blog
+                        </li>
+                        <li className="w-36 py-4 font-normal text-2xl text-center text-purple-900 hover:text-white hover:bg-purple-900">
+                            Contact
+                        </li>
+                        <li className="w-36 py-4 font-normal text-2xl text-center text-purple-900 hover:text-white hover:bg-purple-900">
+                            Sign In
+                        </li>
                     </nav>
                 </section>
-                <section className={styles.titles}>
-                    <h1>Design Matters</h1>
-                    <h3>Entrust us with your digital appearance</h3>
+                <section className="mt-14">
+                    <h1 className="h-20 m-0 mb-1 font-bold text-7xl text-purple-800">
+                        Design Matters
+                    </h1>
+                    <h3 className="h-14 m-0 font-normal text-5xl text-purple-600">
+                        Entrust us with your digital appearance
+                    </h3>
                 </section>
             </main>
         </header>
